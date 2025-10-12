73567af46d1df179a215ac01c3161416631c5324
Leandro França <geoleandro.franca@gmail.com>
2025-10-11 21:47:15 -0300

    Update createTemplate.py

--- algorithms/createTemplate.py
+++ algorithms/createTemplate.py
@@ -236,7 +236,7 @@ class createTemplate(QgsProcessingAlgorithm):
 			z = float(feat1.geometry().constGet().z())
 			if str(z) == 'nan' or z == 0:
 				raise QgsProcessingException('Cota Z não preenchida ou igual a zero no ponto de coordenadas na feição de id {}!'.format(feat1.id()))
-			if z > 3000 or z < -10:
+			if z > 3000 or z < 0:
 				raise QgsProcessingException('Cota Z com valor "{}" na feição de id {} fora dos limites permitidos!'.format(z, feat1.id()))
 
 	def dd2dms(self, dd, n_digits=3):
