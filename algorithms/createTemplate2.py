58e4bfa015f522dc6cccd64e83f39d965452bf87
Leandro França <geoleandro.franca@gmail.com>
2026-03-19 15:26:32 -0300

    Update createTemplate2.py

--- algorithms/createTemplate2.py
+++ algorithms/createTemplate2.py
@@ -92,7 +92,7 @@ class createTemplate2(QgsProcessingAlgorithm):
 					  </div>
 					  <div align="right">
 					  <p align="right">
-					  <a href="https://geoone.com.br/pvgeoincra2/"><span style="font-weight: bold;">Conheça o curso de GeoINCRA no QGIS</span></a>
+					  <a href="https://geoone.com.br/pvgeoincra/"><span style="font-weight: bold;">Conheça o curso de GeoINCRA no QGIS</span></a>
 					  </p>
 					  <p align="right">
 					  <a href="https://portal.geoone.com.br/m/lessons/georreferenciamento-de-imveis-rurais-com-o-plugin-geoincra-1690158094835"><span style="font-weight: bold;">Acesse seu curso na GeoOne</span></a>
