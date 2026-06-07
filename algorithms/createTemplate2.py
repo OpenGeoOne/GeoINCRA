f0760dac5ed56c22b4ec23052ec84b17ae6a008f
Leandro França <geoleandro.franca@gmail.com>
2026-06-07 20:14:37 -0300

    Update createTemplate2.py

--- algorithms/createTemplate2.py
+++ algorithms/createTemplate2.py
@@ -29,11 +29,8 @@ __copyright__ = '(C) 2024 by Tiago Prudencio e Leandro França'
 from qgis.PyQt.QtCore import QCoreApplication
 from qgis.core import (QgsProcessing,
 					   QgsProcessingException,
-					   QgsGeometry,
 					   QgsProcessingParameterNumber,
-					   QgsExpressionContextUtils,
 					   QgsProcessingParameterBoolean,
-					   QgsExpressionContext,
 					   QgsProcessingParameterFeatureSource,
 					   QgsFeatureRequest,
 					   QgsProcessingAlgorithm,
@@ -41,7 +38,6 @@ from qgis.core import (QgsProcessing,
 
 from math import floor, modf
 from qgis.PyQt.QtGui import QIcon
-from GeoINCRA.images.Imgs import *
 import subprocess
 import os, re
 import shutil
@@ -69,38 +65,38 @@ class createTemplate2(QgsProcessingAlgorithm):
 		return 'GeoRural2ODS'
 
 	def displayName(self):
-		return self.tr('GeoRural para Planilha ODS')
+		return self.tr('c. GeoRural para Planilha ODS')
 
 	def group(self):
-		return self.tr(self.groupId())
+		return self.tr('3. 📋 Preenchimento da Planilha ODS')
 
 	def groupId(self):
-		return ''
+		return 'preenchimento_planilha_ods'
 
 	def icon(self):
 		return QIcon(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images/geoincra_pb.png'))
 
 	def tags(self):
 		return 'GeoOne,GeoRural,INCRA,Sigef,memorial,ODS,planilha,conversão,tranformar,descritivo,documento,regularização,fundiária'.split(',')
-
+	
 	def shortHelpString(self):
 		txt = "Cria uma planilha ODS do SIGEF, preenchendo-a diretamente através de Macros, a partir das camadas do banco de dados GeoRural trabalhado no QGIS."
 
 		footer = '''<div>
-					  <div align="center">
-					  <img style="width: 100%; height: auto;" src="data:image/jpg;base64,'''+ INCRA_GeoOne +'''
-					  </div>
-					  <div align="right">
-					  <p align="right">
-					  <a href="https://geoone.com.br/pvgeoincra/"><span style="font-weight: bold;">Conheça o curso de GeoINCRA no QGIS</span></a>
-					  </p>
-					  <p align="right">
-					  <a href="https://portal.geoone.com.br/m/lessons/geoincra?classId=5808"><span style="font-weight: bold;">Acesse seu curso na GeoOne</span></a>
-					  </p>
-					  <a target="_blank" rel="noopener noreferrer" href="https://geoone.com.br/"><img height="80" title="GeoOne" src="data:image/png;base64,'''+ GeoOne +'''"></a>
-					  <p><i>"Mapeamento automatizado, fácil e direto ao ponto é na GeoOne!"</i></p>
-					  </div>
-					</div>'''
+                      <div align="center">
+                      <a target="_blank" rel="noopener noreferrer" href="https://geoone.com.br/pvgeoincra/"><img title="Inscreva-se no curso de GeoINCRA" style="width: 100%; height: auto;" src="'''+ os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images/INCRA_GeoOne.jpg') +'''"></a>
+                      </div>
+                      <div align="right">
+                      <p align="right">
+                      <a href="https://geoone.com.br/pvgeoincra/"><span style="font-weight: bold;">Conheça o curso de GeoINCRA no QGIS</span></a>
+                      </p>
+                      <p align="right">
+                      <a href="https://portal.geoone.com.br/m/lessons/geoincra?classId=5808"><span style="font-weight: bold;">Acesse a aula sobre esta ferramenta no curso de GeoINCRA no GeoOne</span></a>
+                      </p>
+                      <a target="_blank" rel="noopener noreferrer" href="https://geoone.com.br/"><img title="GeoOne" width="280"  src="'''+ os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images/GeoOne.png') +'''"></a>
+                      <p><i>"Mapeamento automatizado, fácil e direto ao ponto é na GeoOne!"</i></p>
+                      </div>
+                    </div>'''
 		return txt + footer
 
 	def initAlgorithm(self, config=None):
