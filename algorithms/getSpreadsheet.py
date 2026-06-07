04585123882fe9eab34b92cf563b7d8c6a472ce9
Leandro França <geoleandro.franca@gmail.com>
2026-06-07 20:14:50 -0300

    Update getSpreadsheet.py

--- algorithms/getSpreadsheet.py
+++ algorithms/getSpreadsheet.py
@@ -31,7 +31,6 @@ from qgis.core import (QgsProcessingException,
                        QgsProcessingAlgorithm,
                        QgsProcessingParameterFileDestination)
 from qgis.PyQt.QtGui import QIcon
-from GeoINCRA.images.Imgs import *
 import os
 import shutil
 import requests
@@ -51,35 +50,35 @@ class getSpreadsheet(QgsProcessingAlgorithm):
         return 'modeloplanilha'
 
     def displayName(self):
-        return self.tr('Baixar planilha ODS do SIGEF')
+        return self.tr('a. Baixar planilha ODS do SIGEF')
 
     def group(self):
-        return self.tr(self.groupId())
+        return self.tr('3. 📋 Preenchimento da Planilha ODS')
 
     def groupId(self):
-        return ''
+        return 'preenchimento_planilha_ods'
 
     def icon(self):
         return QIcon(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images/geoincra_pb.png'))
     
     def tags(self):
         return 'GeoOne,GeoRural,INCRA,Sigef,planilha,ODS,baixar,download,baixar,regularização,fundiária'.split(',')
-
+    
     def shortHelpString(self):
-        txt = "Gera uma planilha ODS vazia para preenchimento com os dados gerados no TXT."
+        txt = "Gera uma planilha ODS vazia para preenchimento com os dados exportados com a ferramenta TXT para planilha ODS."
 
         footer = '''<div>
                       <div align="center">
-                      <img style="width: 100%; height: auto;" src="data:image/jpg;base64,'''+ INCRA_GeoOne +'''
+                      <a target="_blank" rel="noopener noreferrer" href="https://geoone.com.br/pvgeoincra/"><img title="Inscreva-se no curso de GeoINCRA" style="width: 100%; height: auto;" src="'''+ os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images/INCRA_GeoOne.jpg') +'''"></a>
                       </div>
                       <div align="right">
                       <p align="right">
                       <a href="https://geoone.com.br/pvgeoincra/"><span style="font-weight: bold;">Conheça o curso de GeoINCRA no QGIS</span></a>
                       </p>
                       <p align="right">
-                      <a href="https://portal.geoone.com.br/m/lessons/geoincra?classId=2230"><span style="font-weight: bold;">Acesse seu curso na GeoOne</span></a>
+                      <a href="https://portal.geoone.com.br/m/lessons/geoincra?classId=2230"><span style="font-weight: bold;">Acesse a aula sobre esta ferramenta no curso de GeoINCRA no GeoOne</span></a>
                       </p>
-                      <a target="_blank" rel="noopener noreferrer" href="https://geoone.com.br/"><img height="80" title="GeoOne" src="data:image/png;base64,'''+ GeoOne +'''"></a>
+                      <a target="_blank" rel="noopener noreferrer" href="https://geoone.com.br/"><img title="GeoOne" width="280"  src="'''+ os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images/GeoOne.png') +'''"></a>
                       <p><i>"Mapeamento automatizado, fácil e direto ao ponto é na GeoOne!"</i></p>
                       </div>
                     </div>'''
